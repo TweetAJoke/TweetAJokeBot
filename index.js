f0a5f5661a27cf3b1f163b5898a52fb5dc911635
Yann IRBAH <yann.irbah@gmail.com>
2014-11-22 18:04:27 +0100

    Adds homepage

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Ride On Babe!!!!!!");
+var express     = require('express');
+var app         = express();
+var request     = require('request').defaults({ encoding: null });
+var bodyParser  = require('body-parser');
+var path        = require('path');
+
+
+// Application configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+
+
+/////////////////////////
+// Routes definitions //
+////////////////////////
+
+// Homepage
+app.get('/', function(req, res) {
+  res.render('index');
+})
+
+// Fire up the web server
+var port = Number(process.env.PORT || 3000);
+var server = app.listen(port, function() {
+  console.log("TweetAJoke started and listening on port " + port);
+});
